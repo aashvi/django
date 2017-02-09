d7ac640e3452d994d82bdf12db99f7c25db764d3
ayush <asvi1010@gmail.com>
2017-02-09 10:35:53 +0800

    first commit

---
+++ src/signups/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, render_to_response, RequestContext
+
+from .forms import SignUpForm
+
+# Create your views here.
+def home(request):
+	form = SignUpForm()
+	return render_to_response("signup.html",
+							 locals(), context_instance=RequestContext(request))
